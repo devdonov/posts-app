b84760719b637a3ac6d1987b1b942247be8a994f
Ruslan Menibaev <ruslanmenibaev@gmail.com>
2020-09-16 12:21:43 +0200

    Add basic pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from "react-router-dom";
+import { Home, User } from './pages';
 
 function App() {
   return (
-    <div>
-      <h1>Hello Posts!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/user/:id">
+          <User />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
