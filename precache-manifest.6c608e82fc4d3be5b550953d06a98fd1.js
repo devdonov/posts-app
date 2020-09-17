f7b482e2e9712797d2818245640fc29988fbdd44
Ruslan Menibaev <rm.dev.donov@gmail.com>
2020-09-17 14:16:25 +0200

    Updates

---
+++ precache-manifest.6c608e82fc4d3be5b550953d06a98fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c40a8e6059ffdc0876fef4268bf719",
+    "url": "/posts-app/index.html"
+  },
+  {
+    "revision": "5643c66cf5e3d5e3af35",
+    "url": "/posts-app/static/css/main.7cbdd666.chunk.css"
+  },
+  {
+    "revision": "a14eac4f1ed64a48483c",
+    "url": "/posts-app/static/js/2.1abbca8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/posts-app/static/js/2.1abbca8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5643c66cf5e3d5e3af35",
+    "url": "/posts-app/static/js/main.932b226c.chunk.js"
+  },
+  {
+    "revision": "e1280c3ac3e48410721a",
+    "url": "/posts-app/static/js/runtime-main.44c2cf52.js"
+  }
+]);
